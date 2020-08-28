87d8f3c5c1f6beee1f000cea467baaccb71ea756
Ian Rones <ronesp.85@gmail.com>
2020-08-28 14:58:50 -0700

    push

--- cloning/src/components/Landing_Page/Landing.js
+++ cloning/src/components/Landing_Page/Landing.js
@@ -1,26 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import gif from './LandingPageGif.gif'
+import './Landing.css'
+// import gif from './LandingPageGif.gif'
 import logo from './VillageLogo.png'
 
-export default function Landing() {
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer'
 
+export default function Landing() {
     return (
-        <div>
-            <div></div>
-            <div className="">
-                <div className="login">
-                    <div className="navbar">
-                    Navigation Bar
+        <section>
+            <div className="mainpage">
+                    <div className="navbar level">
+                    <Navbar />
                     </div>
+                <div className="login">
                     <header className="main">
-                        <img src={logo} alt="logo image"/>
-                        <img src={gif} alt="moving background"/>
+                        <img className="logo"src={logo} alt="logo image"/>
+                        {/* <img className="background" src={gif} alt="moving background"/> */}
                     </header>
 
-                    <div className="Footer"></div>
+                    <div className="Footer">
+                        <Footer />
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
